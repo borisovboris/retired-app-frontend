ebdc79e6de8ffcb2e31ee981538653a99cdaf21b
borisovboris <boris.s.borisov99@gmail.com>
2021-02-23 20:45:30 +0300

    added subjects component

--- src/app/teacher/teacher-routing.module.ts
+++ src/app/teacher/teacher-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubjectsComponent } from './subjects/subjects.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', children: [
+    {path: '', component: SubjectsComponent, pathMatch: 'full'}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/teacher/teacher.module.ts
+++ src/app/teacher/teacher.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TeacherRoutingModule } from './teacher-routing.module';
+import { SubjectsComponent } from './subjects/subjects.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SubjectsComponent],
   imports: [
     CommonModule,
     TeacherRoutingModule
