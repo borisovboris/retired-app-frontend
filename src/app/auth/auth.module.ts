ec9c0a91cdcb7663ec84b4d8965eab9f0ad8e300
borisovboris <boris.s.borisov99@gmail.com>
2021-03-03 15:44:10 +0300

    added register form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { TeacherLoginComponent } from './teacher-login/teacher-login.component';
 import { TeacherRegisterComponent } from './teacher-register/teacher-register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [TeacherLoginComponent, TeacherRegisterComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/teacher-register/teacher-register.component.ts
+++ src/app/auth/teacher-register/teacher-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-teacher-register',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher-register.component.css']
 })
 export class TeacherRegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    repeatPassword: new FormControl('', [Validators.required])
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.controls);
   }
 
 }
